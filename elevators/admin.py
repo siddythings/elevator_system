070567527c9ea38e3b0ad504a7904e2c60ca555c
siddhart <siddharthavikram9876@gmail.com>
2022-12-18 07:59:25 +0530

    elevator maintance mondel and migrations

--- elevators/admin.py
+++ elevators/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Elevator, Request, MaintenanceStatus
 
 # Register your models here.
+admin.site.register(Elevator)
+admin.site.register(Request)
+admin.site.register(MaintenanceStatus)
--- elevators/models.py
+++ elevators/models.py
@@ -21,3 +21,14 @@ class Request(models.Model):
 
     def __str__(self):
         return "%s, %s"(self.elevator, self.updated_at)
+
+
+class MaintenanceStatus(models.Model):
+    elevator = models.OneToOneField(Elevator, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20)
+    reason = models.CharField(max_length=100)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%s, %s"(self.elevator, self.updated_at)
