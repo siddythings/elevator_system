5a7f8c3180a5c25b1521cadfc48629608b3e62ad
siddhart <siddharthavikram9876@gmail.com>
2022-12-18 07:48:52 +0530

    create: request modele

--- elevators/models.py
+++ elevators/models.py
@@ -10,3 +10,14 @@ class Elevator(models.Model):
 
     def __str__(self):
         return "%s, %s"(self.id, self.updated_at)
+
+
+class Request(models.Model):
+    elevator = models.ForeignKey(Elevator, on_delete=models.CASCADE)
+    floor = models.PositiveIntegerField()
+    direction = models.CharField(max_length=5)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%s, %s"(self.elevator, self.updated_at)
