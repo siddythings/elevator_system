0fce4d6f2136f516fce2ffe56ee986cc6886d0c3
siddhart <siddharthavikram9876@gmail.com>
2022-12-18 07:42:18 +0530

    create: elevator models define

--- elevators/models.py
+++ elevators/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Elevator(models.Model):
+    current_floor = models.PositiveIntegerField()
+    direction = models.CharField(max_length=5)
+    maintenance_status = models.CharField(max_length=20)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%s, %s"(self.id, self.updated_at)
